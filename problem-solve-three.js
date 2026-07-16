630dc6c6fae675a792353ccc6d7ad00e7c8cc633
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 14:49:10 +0600

    new problem solved

---
+++ problem-solve-three.js
@@ -0,0 +1,9 @@
+/**Create a variable
+ *  isRaining (true/false). If it is true,
+ *  print "Take an umbrella". */
+
+
+const isRaining = true;
+if(isRaining==true){
+    console.log('Take an umbrella');
+}
