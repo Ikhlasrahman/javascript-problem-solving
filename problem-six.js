f7828ed9ac742ff060aaa61a6798b74d52e5cc9f
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 15:26:16 +0600

    new problem solved

---
+++ problem-six.js
@@ -0,0 +1,9 @@
+/**Take a year variable
+ * and check whether it is a leap year (hint: year % 4 === 0) */
+
+const year = 2024;
+if (year % 4 === 0) {
+  console.log("Leap year");
+} else {
+  console.log("Not a leap year");
+}
---
+++ problem-solve-five.js
@@ -0,0 +1,10 @@
+/**Take a number variable and check whether 
+ * it is positive or negative (using if-else). */
+
+const number = 20;
+
+if(number > 0){
+    console.log('number is positive');
+} else{
+    console.log('number is negative');
+}
