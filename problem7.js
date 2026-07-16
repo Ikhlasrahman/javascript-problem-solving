8f065f5d4bdcb764675a048ae4f001329d1df531
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 15:43:27 +0600

    New changes added

---
+++ problem7.js
@@ -0,0 +1,10 @@
+/**Take a speed variable.
+ *  If speed is greater than 80, 
+ * print "Overspeeding", otherwise print "Normal speed" */
+
+const speed = 150;
+if(speed>80){
+    console.log('Overspeeding');
+}else{
+    console.log('Normal Speed');
+}
