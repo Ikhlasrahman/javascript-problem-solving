537927157b658ec0cc7a27b6d5c76d451ef89cf0
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 14:34:00 +0600

    js practice work

---
+++ problem-solve-one.js
@@ -0,0 +1,15 @@
+/** Take two variables price1 and price2,
+ * and compare them using all comparison operators
+ *  (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log. */
+
+const price1 = 50;
+const price2 = 100;
+
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 < price2);
+console.log(price1 >= price2);
+console.log(price1 <= price2);
