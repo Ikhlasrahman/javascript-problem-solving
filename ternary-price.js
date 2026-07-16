18f18d4a5cb8f47597aa356dd3e1771c0d276ba6
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 18:12:54 +0600

    ternary price problem solved

---
+++ ternary-price.js
@@ -0,0 +1,7 @@
+/**If the price is greater than 1000, 
+ * print "Expensive", otherwise "Affordable" 
+ * — write this using a ternary operator. */
+
+const price = 5000;
+
+console.log(price>1000?"Expensive":"Affortable");
