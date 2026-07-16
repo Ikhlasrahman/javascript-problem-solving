1c6333b5fe6e08dd267b8689c85a7f46eda17cec
ikhlasrahmanmimo <ikhlasrahmanmimo@gmail.com>
2026-07-16 14:43:06 +0600

    my guess

---
+++ problem-solve-two.js
@@ -0,0 +1,7 @@
+/**Guess the result of 
+ * "5" == 5 and "5" === 5 first, then write code to verify your guess. */
+
+//My Guess "5"==5 is false
+//My Guess "5"===5 is false
+console.log("5"==5);
+console.log("5"===5);
