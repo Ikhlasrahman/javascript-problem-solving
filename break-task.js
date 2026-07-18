664744db852ec1f8d926fe4ccf86a77a892e40b9
Ikhlas <ikhlas@Ikhlass-MacBook-Air.local>
2026-07-18 19:40:22 +0600

    break task

---
+++ break-task.js
@@ -0,0 +1,8 @@
+//Write a for loop from 1 to 50 that stops completely (break) 
+// as soon as it reaches a number greater than 30.
+
+for(let i=0;i<=50;i++){
+    if(i>30)
+    {break;
+    } console.log(i);
+}
