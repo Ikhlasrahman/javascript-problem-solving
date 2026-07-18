0dbb79d92c2a49cb7dff8299c27dbc760f1911e3
Ikhlas <ikhlas@Ikhlass-MacBook-Air.local>
2026-07-18 23:54:46 +0600

    loop array task

---
+++ loopArray.js
@@ -0,0 +1,14 @@
+//11. Loop through the array with a for loop and log each item.
+const color = ['Red','Green','Blue','yellow','pink','light blue'];
+
+// For Loop
+// for(let i=0;i<color.length;i++){
+//     console.log(color[i]);
+// }
+
+//while loop
+let i=0;
+while(i<color.length){
+    console.log(color[i]);
+    i++
+}
